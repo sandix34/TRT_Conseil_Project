3c2d140e66c6a3c5842ba59f8aa929cfcdfa20b7
sandix34 <sandrinezocca@gmail.com>
2022-02-22 17:30:13 +0100

    refactoring the database connection

--- server/app.js
+++ server/app.js
@@ -3,8 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 require('dotenv').config()
-const mongoose = require('mongoose');
-const db = process.env.MONGODB_CONNECT;
+require("./config/database").connect();
 const index = require('./routes');
 
 const app = express();
@@ -16,10 +15,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(index);
-
-// Database connection
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then( () => console.log('connexion db OK') )
-    .catch( err => console.log(err) );
 
 module.exports = app;
